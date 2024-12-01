270cf798779a61eb3452badb803d62b12beefb60
aliw14 <aligm@code.edu.az>
2024-12-01 06:04:45 +0400

    bgImage

--- src/screens/Welcome.Screen.tsx
+++ src/screens/Welcome.Screen.tsx
@@ -20,7 +20,10 @@ export const WelcomeScreen: React.FC<
   };
 
   return (
-    <ImageBackground style={styles.background} resizeMode="contain">
+    <ImageBackground
+      style={styles.background}
+      source={require('../assets/images/backgroundImage.png')}
+      resizeMode="contain">
       <View style={styles.main}>
         <Image
           style={styles.departmentLogo}
